7f567614adf8006d497276b8647f08d79a7f4460
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-21 17:48:25 -0400

    Fixed repo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route } from "react-router-dom";
+
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
@@ -13,7 +13,6 @@ const App = () => {
 
   return (
     <div>
-      <Route to="/" component={MovieList} />
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
     </div>
